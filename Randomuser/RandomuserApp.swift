f36c0bd5729824f941b9dc9cfbaef48236969a0f
deshabml <deshab@yandex.ru>
2023-05-16 10:59:41 +0300

    Initial Commit

---
+++ Randomuser/RandomuserApp.swift
@@ -0,0 +1,17 @@
+//
+//  RandomuserApp.swift
+//  Randomuser
+//
+//  Created by Лаборатория on 16.05.2023.
+//
+
+import SwiftUI
+
+@main
+struct RandomuserApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
